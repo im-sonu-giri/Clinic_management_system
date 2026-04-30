44a56d21afec993ed979cbcee611e3e39ced013c
im-sonu-giri <124750682+im-sonu-giri@users.noreply.github.com>
2026-04-30 07:54:51 +0545

    book appointment API created

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -3,6 +3,8 @@ import bcrypt from 'bcrypt'
 import userModel from '../models/userModel.js'
 import jwt from 'jsonwebtoken'
 import { v2 as cloudinary } from "cloudinary"
+import doctorModel from '../models/doctorModel.js'
+import appointmentModel from '../models/appointmentModel.js'
 
 // api to registr user
 const registerUser = async (req, res) => {
@@ -135,20 +137,20 @@ const updateProfile = async (req, res) => {
     try {
         const { userId, name, phone, address, dob, gender } = req.body
         const imageFile = req.file
-        if (!name || !phone ||  !dob || !gender) {
+        if (!name || !phone || !dob || !gender) {
             return res.status(400).json({
                 success: false,
                 message: "Data missing"
             })
-            await userModel.findByIdAndUpdate(userId, {name, phone, address: JSON.parse(address), dob, gender })
-            if(imageFile){
+            await userModel.findByIdAndUpdate(userId, { name, phone, address: JSON.parse(address), dob, gender })
+            if (imageFile) {
                 //upload image to cloudinary
 
-                const imageUpload = await cloudinary.uploader.upload(imageFile.path, {resource_type:'image'})
+                const imageUpload = await cloudinary.uploader.upload(imageFile.path, { resource_type: 'image' })
                 const imageURL = imageUpload.secure_url
-                await userModel.findByIdAndUpdate(userId, {image: imageURL})
+                await userModel.findByIdAndUpdate(userId, { image: imageURL })
             }
-            res.json({success: true, message: "profile updated"})
+            res.json({ success: true, message: "profile updated" })
         }
 
     } catch (error) {
@@ -161,6 +163,66 @@ const updateProfile = async (req, res) => {
     }
 
 }
+// API to book appointment
+const bookAppointment = async (req, res) => {
+    try {
+        const { userId, docId, slotDate, slotTime } = req.body
+        const docData = await doctorModel.findById(docId).select('-password')
+
+        if (!docData.available) {
+            return res.json({
+                success: false,
+                message: 'Doctor not available'
+            });
+        }
+        let slots_booked = docData.slots_booked
+        if (slots_booked[slotDate]) {
+            // check if time already booked
+            if (slots_booked[slotDate].includes(slotTime)) {
+                return res.json({
+                    success: false,
+                    message: 'Slot already booked'
+                });
+            } else {
+                // add new time to existing date
+                slots_booked[slotDate].push(slotTime);
+            }
+        } else {
+            // create new date entry
+            slots_booked[slotDate] = []
+            slots_booked[slotDate].push(slotTime)
+        }
+        const userData = await userModel.findById(userId).select('-password')
+
+        delete docData.slots_booked
+
+        const appointmentData = {
+            userId,
+            docId,
+            userData,
+            docData,
+            amount: docData.fees,
+            slotTime,
+            slotDate,
+            date: Date.now()
+        };
+        const newAppointment = new appointmentModel(appointmentData)
+        await newAppointment.save()
+
+        // save new slots data in docData
+        await doctorModel.findByIdAndUpdate|(docId,{slots_booked})
+        res.json({success:true, message:'Appointment Book'})
 
 
-export { registerUser, loginUser, getProfile, updateProfile }
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            success: false,
+            message: error.message
+        })
+
+    }
+}
+
+
+export { registerUser, loginUser, getProfile, updateProfile, bookAppointment }
