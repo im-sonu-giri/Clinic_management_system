8493049e1e71c2c4df20951e0a9bfe75474b803e
im-sonu-giri <124750682+im-sonu-giri@users.noreply.github.com>
2026-05-02 17:11:41 +0545

    API created for the doctor login

--- Backend/controllers/doctorController.js
+++ Backend/controllers/doctorController.js
@@ -1,4 +1,6 @@
 import doctorModel from "../models/doctorModel.js"
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
 
 const changeAvailability = async (req, res) => {
     try {
@@ -46,5 +48,31 @@ const doctorList = async (req, res) => {
         })
     }
 }
+// Api for doctor login
+const loginDoctor = async(req, res) =>{
+    try {
+        const {email, password} = req.body
+        const doctor = await doctorModel.findOne({email})
+        if(!doctor){
+            return res.json({success:false, message:"Invalid Crendientials"})
+        }
+        const isMatch = await bcrypt.compare(password, doctor.password)
+        if(isMatch){
+            const token = jwt.sign({id:doctor._id},process.env.JWT_SECRET)
+            res.json ({success:true, token})
+        }else{
+            return res.json({success:false, message:"Invalid Crendientials"})
 
-export { changeAvailability, doctorList }
+        }
+        
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            success: false,
+            message: error.message
+        })
+        
+    }
+}
+
+export { changeAvailability, doctorList, loginDoctor }
