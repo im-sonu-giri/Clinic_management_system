445d69362f49c9bf2c2316c31fb6f75a7688ae76
im-sonu-giri <124750682+im-sonu-giri@users.noreply.github.com>
2026-05-03 13:59:44 +0545

    all admin integration completed

--- admin/src/pages/Admin/Appointments.jsx
+++ admin/src/pages/Admin/Appointments.jsx
@@ -143,8 +143,9 @@ const Appointments = () => {
                   <span className='text-red-500 text-sm font-medium'>
                     Cancelled
                   </span>
-                 : <img 
-                 onClick={()=>cancelAppointment(item._id)}
+                 : item.isCompleted ?
+                 <p className='text-green-500 text-sm font-medium'>Completed</p>
+                 :<img onClick={()=>cancelAppointment(item._id)}
                 className='w-10 cursor-pointer'
                 src={assets.cancel_icon} alt="" />
                 }
--- admin/src/pages/Admin/Dashboard.jsx
+++ admin/src/pages/Admin/Dashboard.jsx
@@ -8,7 +8,7 @@ import { AppContext } from '../../context/AppContext'
 const Dashboard = () => {
 
   const { aToken, getDashData, cancelAppointment, dashData } = useContext(AdminContext)
-  const {slotDateFormat} =useContext(AppContext)
+  const { slotDateFormat } = useContext(AppContext)
   useEffect(() => {
     if (aToken) {
       getDashData()
@@ -57,10 +57,10 @@ const Dashboard = () => {
         <div className='pt-4 border border-t-0'>
           {
             dashData.latestAppointments.map((item, index) => {
-              <div className='flex items-center px-6 py-3 gap-3 hover:bg-gray-100'  key={index}>
-                <img 
-                className='rounded-full w-10'
-                src={item.docData.image} alt="" />
+              <div className='flex items-center px-6 py-3 gap-3 hover:bg-gray-100' key={index}>
+                <img
+                  className='rounded-full w-10'
+                  src={item.docData.image} alt="" />
                 <div className='flex-1 text-sm'>
                   <p className='text-gray-800 font-medium'>{item.docData.name}</p>
                   <p className='text-gray-600'>{slotDateFormat(item.slotDate)}</p>
@@ -69,10 +69,11 @@ const Dashboard = () => {
                   <span className='text-red-500 text-sm font-medium'>
                     Cancelled
                   </span>
-                  : <img
-                    onClick={() => cancelAppointment(item._id)}
-                    className='w-10 cursor-pointer'
-                    src={assets.cancel_icon} alt="" />
+                  : item.isCompleted ?
+                    <p className='text-green-500 text-sm font-medium'>Completed</p>
+                    : <img onClick={() => cancelAppointment(item._id)}
+                      className='w-10 cursor-pointer'
+                      src={assets.cancel_icon} alt="" />
                 }
 
               </div>
