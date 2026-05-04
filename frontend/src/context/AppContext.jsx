be42cfeff1bc08651ceaacbba22086cec21be5aa
SONU GIRI <124750682+im-sonu-giri@users.noreply.github.com>
2026-05-04 22:28:50 +0545

    Update AppContext.jsx

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -7,7 +7,7 @@ export const AppContext = createContext()
 
 const AppContextProvider = (props) => {
     const currencySymbol = "Rs."
-    const backendUrl = import.meta.env.VITE_BACKEND_URL
+    const backendUrl = "https://clinic-management-system-server-j1q1.onrender.com"
 
     const [doctors, setDoctors] = useState([])
     const [token, setToken] = useState(localStorage.getItem('token') ? localStorage.getItem('token') : false)
